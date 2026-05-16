d8f01a7dc01623b69f673e2788ae2d6417b52e90
Mourya Manideep Kandregula <kmmd434389@gmail.com>
2026-05-17 00:51:30 +0530

    Fix Kadane loop start and update sample arrays
    Initialize current and max sums from arr[0] and start the Kadane loop at index 1 (previously started at 0), preventing an incorrect reuse of the first element. Also commented out and replaced sample input vectors to use a different test case ({-1, -2, 3, 4, -5}).

--- 01_arrays/subarrayusekadanes.cpp
+++ 01_arrays/subarrayusekadanes.cpp
@@ -6,10 +6,12 @@ using namespace std;
 int main(){
     int n=7;
     // int arr[] = {-5, 1, -2, 3, -1, 2, -2};
-    vector<int> arr = {-3, 8, -2, 4, -5, 6};
+    // vector<int> arr = {-3, 8, -2, 4, -5, 6};
+    // vector<int> arr = {5,4,-1,7,8};
+    vector<int> arr = {-1, -2, 3, 4, -5};
     int cs=arr[0], maxsum=arr[0];
 
-    for(int i=0; i<arr.size(); i++)
+    for(int i=1; i<arr.size(); i++)
     {
         cs = max(arr[i], cs + arr[i]);
         maxsum = max(cs, maxsum);
